ca2b9a0ad91238ffedd4b5d4e1876a2cfe602518
Kellocx <marialessandro79@gmail.com>
2025-11-14 15:55:06 +0100

    terzo

---
+++ TRIP_AGENCY/footer.php
@@ -0,0 +1,17 @@
+</main>
+
+<footer class="text-white text-center mt-auto " style="background-color: #0081a7">
+    <small>©Trip Agency <?php echo date("Y"); ?></small>
+
+
+
+
+
+
+
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ TRIP_AGENCY/index.php
@@ -0,0 +1,10 @@
+<?php include 'header.php' ?>
+
+<div class="text-center py-4">
+<h1 class="disply-5">Benvenuta nella nostra Trip Agency</h1>
+<p class="lead">Esplora le nostre destinazioni, gestisci i clienti emonitora le prenotazioni</p>
+</div>
+<div></div>
+
+
+<?php include 'footer.php' ?>
