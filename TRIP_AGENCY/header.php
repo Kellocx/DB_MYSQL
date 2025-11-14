66a66156d1dcf550a803facd0d6ac278cc11d748
Kellocx <marialessandro79@gmail.com>
2025-11-14 15:30:18 +0100

    second commit

--- TRIP_AGENCY/header.php
+++ TRIP_AGENCY/header.php
@@ -8,8 +8,40 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1></h1>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0081a7;">
+        <div class="container-fluid">
+
+            <!-- logo -->
+            <a class="navbar-brand d-flex align-items-center" href="index.php"></a>
+            <img src="./logo.png" alt="" style="height:50px; width:auto;">
+            Trip-Agency
+            </a>
+
+            <button class="navbar-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggle-icon"></span>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="vabarNav">
+
+                <div class="navbar-nav">
+                    <a href="clienti.php" class="nav-link">Clienti</a>
+                    <a href="destinazioni.php" class="nav-link">Destinazioni</a>
+                    <a href="prenotazioni.php" class="nav-link">Prenotazioni</a>
+                    <a href="ricerca.php" class="nav-link">Ricerca</a>
+                    <a href="statistiche.php" class="nav-link">Statistiche</a>
+
+                </div>
+
+            </div>
+
+            <!--- link menu -->
+
+        </div>
+
+    </nav>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
